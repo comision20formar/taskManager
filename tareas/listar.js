122229581ee4c4cd6e11c3f8449a243738b80cd6
GonzaloArmoa <gonzalo-armoa@hotmail.com>
2023-05-30 20:51:32 -0300

    Add listar.js to tareas

---
+++ tareas/listar.js
@@ -0,0 +1,8 @@
+const listar = function (tareas = this.tareas) {
+    tareas.forEach(({ clase, titulo, estado }) => {
+      console.log(`CLASE: ${clase}: ${titulo} --> ${estado}`);
+    })
+}
+module.exports = {
+    listar
+}
